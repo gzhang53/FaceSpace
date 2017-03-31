646a82aca3bf11889dd5c2ec962c171eda1f3732
zhangguanpeng <gzhang53@uwo.ca>
2017-03-31 01:55:06 -0400

    React Tutorial

--- src/main/js/accountcreation.js
+++ src/main/js/accountcreation.js
@@ -6,10 +6,41 @@ export class AccountCreation extends React.Component {
         super();
     }
 
+
+    handleSubmit(e) {
+        // Prevents reinitialization
+        e.preventDefault();
+        let name = this.state.name;
+
+        fetch('http://localhost:8080/UserAccount/createAccount?'
+            + 'userName=' + name , {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(res =>{
+            if(res.ok){
+                this.setState({success: 'Successfully created'});
+            }
+            else{
+                this.setState({success: 'Already exists'});
+            }
+        })
+    }
+
     render() {
         return(
             <div>
-                Account creation elements go here.
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        <input type="text"  />
+                    </label>
+
+                    <input type="submit" value="Create Account!" />
+                </form>
+                Name: {this.state.name}
+                <br/>
+                Message: {this.state.name} + {this.handleSubmit}
             </div>
         );
     }
